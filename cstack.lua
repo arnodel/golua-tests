db8f72cb0b92d8f1c9d2093b3d1e236e7fa77dcf
Arnaud Delobelle <arnodel@gmail.com>
2022-01-17 11:40:19 +0000

    Adapt cstack

--- cstack.lua
+++ cstack.lua
@@ -42,7 +42,7 @@ do  print("testing recursion inside pattern matching")
   end
   local m = f(80)
   assert(#m == 80)
-  checkerror("too complex", f, 2000)
+  checkerror("too complex", f, 20000)
 end
 
 
@@ -66,7 +66,10 @@ do  print("testing stack-overflow in recursive 'gsub'")
   print("final count: ", count)
 end
 
-
+-- These tests would not work on Golua because it doesn't consume stack frames
+-- for Lua function.  TODO: consider whether we want to cause an error
+-- "artificially" anyway.
+--[[
 do   -- bug in 5.4.0
   print("testing limits in coroutines inside deep calls")
   local count = 0
@@ -103,7 +106,7 @@ do
   assert(not pcall(f))
   print("final count: ", count)
 end
-
+]]
 
 if T then
   print("testing stack recovery")
